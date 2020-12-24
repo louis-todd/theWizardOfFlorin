526f3d225b3b86a5423a9b7ba678a1b3b036ced2
Chloe <chloetaylor@Cat.local>
2020-12-24 15:15:56 +0000

    Began adding rectangle

--- src/me/ghost/Dialogue.java
+++ src/me/ghost/Dialogue.java
@@ -1,7 +1,9 @@
 package me.ghost;
 
 import org.jsfml.graphics.Font;
+import org.jsfml.graphics.RectangleShape;
 import org.jsfml.graphics.Text;
+import org.jsfml.system.Vector2f;
 
 import java.io.IOException;
 import java.nio.file.Paths;
@@ -17,6 +19,7 @@ public class Dialogue {
         simpleFont = new Font();                                    //Set the font
         toWrite = textToWrite;
         writeText();
+        formatText();
     }
 
     public void writeText(){
@@ -33,6 +36,11 @@ public class Dialogue {
                 this.setPosition(20, 400);
             }};
 
+    }
+
+    public void formatText(){
+        Vector2f dimensions = new Vector2f(3,3);
+        RectangleShape textBackground = new RectangleShape(dimensions);
     }
 
     public Text getText(){
