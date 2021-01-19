214d6d1b06347d3232be5d6acbb7f8fed9cf1395
guygy1 <60134735+guygy1@users.noreply.github.com>
2021-01-19 14:24:22 +0000

    fix merge errors

--- src/me/ghost/Game.java
+++ src/me/ghost/Game.java
@@ -1,210 +1,11 @@
-<<<<<<< HEAD
+
 package me.ghost;
 
-import org.jsfml.graphics.*;
-import org.jsfml.system.Vector2f;
-import org.jsfml.window.Keyboard;
-import org.jsfml.window.VideoMode;
-import org.jsfml.window.event.Event;
-import org.jsfml.window.event.KeyEvent;
-
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.nio.file.Paths;
-import java.util.Arrays;
-
-public class Game {
-    private final RenderWindow window;
-
-    private Character wizard;
-    private Character npc;
-    private boolean RIGHT = false;
-    private boolean LEFT = false;
-    private boolean UP = false;
-    private boolean DOWN = false;
-    private boolean FIRSTSPACE = false;
-    private Text dialogueWindowText;
-    private Dialogue interaction;
-
-    /**
-     * Constructor for the game class
-     */
-    public Game() {
-        //Create the window and set window name to: 'Welcome Wizards'
-        window = new RenderWindow(new VideoMode(640, 480), "Welcome Wizards");
-
-        wizard = new Character(320, 240, 0.05f, "resources/smileyface.png");
-        npc = new Character(250, 300, 0.05f, "resources/smileyface.png");
-
-        //Limit the framerate
-        window.setFramerateLimit(120);
-
-    }
-
-    /**
-     * Runs the window including inputs and updating the window
-     */
-    public void run() {
-        while (window.isOpen()) {
-            handleEvents();
-
-            //instead of moveWizard have draw sprites which draws all sprites in their updated positions
-            moveWizard(wizard);
-
-            isDialogue();
-
-            loadMap();
-
-            updateWindow();
-        }
-    }
-
-    /**
-     * Handles the input events
-     */
-    private void handleEvents(){
-        //Handle events
-        for (Event event : window.pollEvents()) {
-            switch (event.type) {
-                case CLOSED:
-                    window.close();
-                    break;
-                case KEY_RELEASED:
-                    KeyEvent keyRelease = event.asKeyEvent();
-                    if (keyRelease.key == Keyboard.Key.RIGHT || keyRelease.key == Keyboard.Key.LEFT || keyRelease.key == Keyboard.Key.UP || keyRelease.key == Keyboard.Key.DOWN) {
-                        handleKeyPress(keyRelease, false);
-                    }
-                    break;
-                case KEY_PRESSED:
-                    KeyEvent keyEvent = event.asKeyEvent();
-                    if (keyEvent.key == Keyboard.Key.RIGHT || keyEvent.key == Keyboard.Key.LEFT || keyEvent.key == Keyboard.Key.UP || keyEvent.key == Keyboard.Key.DOWN) {
-                        handleKeyPress(keyEvent, true);
-                    }
-
-                    //Special case for when space is pressed
-                    if (keyEvent.key == Keyboard.Key.SPACE) {
-                        handleKeyPress(keyEvent, !FIRSTSPACE);
-                    }
-                    break;
-            }
-        }
-    }
-
-    /**
-     * Moves the wizard if the direction flags are true
-     * @param wizard wizard sprite
-     */
-    private void moveWizard(Sprite wizard) {
-        if (RIGHT) {
-            wizard.move(1, 0);
-        }
-        if (LEFT) {
-            wizard.move(-1, 0);
-        }
-        if (UP) {
-            wizard.move(0, -1);
-        }
-        if (DOWN) {
-            wizard.move(0, 1);
-        }
-    }
-
-    private void isDialogue() {
-
-        //If its the first time space is pressed, set the text
-        if(FIRSTSPACE){
-            Dialogue genericText = new Dialogue("resources/Roboto-Regular.ttf", "Hello Wizards!");
-            dialogueWindowText = genericText.getText();
-            interaction = new Dialogue("resources/Roboto-Regular.ttf", "Hello Wizards!");
-        }
-
-    }
-
-    /**
-     * Updates the window
-     */
-    private void updateWindow(){
-        window.clear(Color.RED);
-
-        window.draw(wizard);
-        window.draw(npc);
-
-        //Only draw if it is the first space
-        if(FIRSTSPACE) {
-            window.draw(dialogueWindowText);
-            interaction.draw(window, null);
-        }
-
-        window.display();
-    }
-
-    /**
-     * Sets direction flags to true if direction key is pressed
-     *
-     * @param movementKey direction key
-     * @param pressed boolean - whether the key is pressed or not
-     */
-    private void handleKeyPress(KeyEvent movementKey, boolean pressed) {
-        switch (movementKey.key) {
-            case RIGHT:
-                RIGHT = pressed;
-                break;
-            case LEFT:
-                LEFT = pressed;
-                break;
-            case UP:
-                UP = pressed;
-                break;
-            case DOWN:
-                DOWN = pressed;
-                break;
-            case SPACE:
-                FIRSTSPACE = pressed;
-                break;
-        }
-    }
-
-    private void loadMap() {
-        File csvFile = new File("resources/map._House.csv");
-        if (csvFile.isFile()) {
-            String row;
-            try {
-                BufferedReader csvReader = new BufferedReader(new FileReader("resources/map._House.csv"));
-                while ((row = csvReader.readLine()) != null) {
-                    String[] data = row.split(",");
-                    int imageNo = Integer.valueOf(data[0]);
-                    switch (imageNo){
-                        case -1:
-                            Texture texture2 = new Texture();
-
-                            try {
-                                //Load the image.
-                                Image image = new Image();
-                                image.loadFromFile(Paths.get("resources/tiles/tile6.png"));
-
-                                //Apply the color mask
-                                //image.createMaskFromColor(Color.BLUE);
-
-                                //Load the masked image into the texture
-                                texture2.loadFromImage(image);
-                            } catch(IOException|TextureCreationException ex) {
-                                System.err.println("Something went wrong:");
-                                ex.printStackTrace();
-                            }
-                        default:
-                            //draw nothing
-                    }
-                }
-                csvReader.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-}
-
-=======
-package me.ghost;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -377,5 +178,43 @@ public class Game {
                 break;
         }
     }
+
+    private void loadMap() {
+        File csvFile = new File("resources/map._House.csv");
+        if (csvFile.isFile()) {
+            String row;
+            try {
+                BufferedReader csvReader = new BufferedReader(new FileReader("resources/map._House.csv"));
+                while ((row = csvReader.readLine()) != null) {
+                    String[] data = row.split(",");
+                    int imageNo = Integer.valueOf(data[0]);
+                    switch (imageNo){
+                        case -1:
+                            Texture texture2 = new Texture();
+
+                            try {
+                                //Load the image.
+                                Image image = new Image();
+                                image.loadFromFile(Paths.get("resources/tiles/tile6.png"));
+
+                                //Apply the color mask
+                                //image.createMaskFromColor(Color.BLUE);
+
+                                //Load the masked image into the texture
+                                texture2.loadFromImage(image);
+                            } catch(IOException|TextureCreationException ex) {
+                                System.err.println("Something went wrong:");
+                                ex.printStackTrace();
+                            }
+                        default:
+                            //draw nothing
+                    }
+                }
+                csvReader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
->>>>>>> master
+
