ad31b4392801b6467dc670ad57c4a109fc2a6d6e
guygy1 <60134735+guygy1@users.noreply.github.com>
2021-01-20 12:40:58 +0000

    Add texture to tile

--- src/me/ghost/map/Tile.java
+++ src/me/ghost/map/Tile.java
@@ -1,10 +1,14 @@
 package me.ghost.map;
 
 import org.jsfml.graphics.RectangleShape;
+import org.jsfml.graphics.Texture;
 import org.jsfml.system.Vector2f;
 
 public class Tile extends RectangleShape {
-    public Tile(Vector2f vector2f) {
+    Texture tileTexture;
+
+    public Tile(Vector2f vector2f, Texture setTexture) {
         super(vector2f);
+        this.tileTexture = setTexture;
     }
 }
