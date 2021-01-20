78b156eaad46bdd6f604069eaa446a5f759637a5
guygy1 <60134735+guygy1@users.noreply.github.com>
2021-01-20 13:52:24 +0000

    Close Buffered Reader

--- src/me/ghost/map/GameMap.java
+++ src/me/ghost/map/GameMap.java
@@ -72,6 +72,7 @@ public class GameMap {
                     rowNumber++;
                 }
             }
+            csvReader.close();
         } catch (IOException ex){
             ex.printStackTrace();
         }
