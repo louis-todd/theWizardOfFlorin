3089fe05f012759b480cf59bc0777497539f3ae7
guygy1 <60134735+guygy1@users.noreply.github.com>
2021-02-08 12:55:21 +0000

    Add 1000 projectiles to stack

--- src/me/ghost/battle/dodge/DodgeGame.java
+++ src/me/ghost/battle/dodge/DodgeGame.java
@@ -5,9 +5,13 @@ import me.ghost.Characters.Npc;
 import me.ghost.battle.BattleWindow;
 import org.jsfml.graphics.ConvexShape;
 
+import java.util.Random;
+import java.util.Stack;
+
 public class DodgeGame{
 
     private Npc battleNpc;
+    Stack<Projectile> projectileStack = new Stack<>();
     private MoveableCharacter wizard;
     public DodgeGame(Npc setBattleNpc, MoveableCharacter setWizard) {
         BattleWindow battleWindow = new BattleWindow();
@@ -16,5 +20,16 @@ public class DodgeGame{
         this.wizard = setWizard;
         this.wizard.setPosition(battleWindow.getPlayerAreaCentre());
 
+        this.addProjectilesToStack(1000);
     }
+
+    private void addProjectilesToStack(int numberOfProjectiles){
+        Random rand = new Random();
+        int minSides = 0;
+        int maxSides = 10;
+        for(int i = 0; i < numberOfProjectiles; i++){
+            projectileStack.push(new Projectile(rand.nextInt(maxSides - minSides + 1)));
+        }
+    }
+
 }
