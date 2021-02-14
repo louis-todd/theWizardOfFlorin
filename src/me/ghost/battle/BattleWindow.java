af442ed27c7ca2846d3c9c6821ac519a25568ee3
guygy1 <60134735+guygy1@users.noreply.github.com>
2021-02-14 16:37:01 +0000

    Draw the columns

--- src/me/ghost/battle/BattleWindow.java
+++ src/me/ghost/battle/BattleWindow.java
@@ -6,38 +6,36 @@ import org.jsfml.graphics.*;
 import org.jsfml.graphics.Drawable;
 import org.jsfml.system.Vector2f;
 
-public class BattleWindow implements Drawable{
+public class BattleWindow implements Drawable {
 
-    private  ArrayList<Drawable> toDraw;
-    private  ArrayList<CircleShape> healthCircles;
-    private  ArrayList<Drawable> getToDraw;
+    private ArrayList<Drawable> toDraw;
+    private ArrayList<CircleShape> healthCircles;
+    private ArrayList<Drawable> getToDraw;
     //private final RectangleShape ghostArea;
-    private  RectangleShape playerArea;
+    private RectangleShape playerArea;
 
-    public BattleWindow(){
+    public BattleWindow() {
         int life = 1;
 
 //        while(life == 1)
 //        {
 
-            toDraw = new ArrayList<>();
-            healthCircles = new ArrayList<>();
-            //columnsRec = new ArrayList<>();
+        toDraw = new ArrayList<>();
+        healthCircles = new ArrayList<>();
+        //columnsRec = new ArrayList<>();
 
-            rectangle(640, 480, 0, 0, 0, 0, 0); //Background
-            //ghostArea = rectangle(500, 180, 70, 10, 255, 255, 255); //Ghost Area
-            playerArea = rectangle(620, 320, 10, 10, 255, 255, 255); //Player Area
-            rectangle(30,20,10, 340,255,30,0); //Health label
+        rectangle(640, 480, 0, 0, 0, 0, 0); //Background
+        //ghostArea = rectangle(500, 180, 70, 10, 255, 255, 255); //Ghost Area
+        playerArea = rectangle(620, 320, 10, 10, 255, 255, 255); //Player Area
+        rectangle(30, 20, 10, 340, 255, 30, 0); //Health label
 
 
+        // for health circles
+        for (int i = 50; i <= 100; i += 25) {
+            healthCircles.add(healthCircle(i, 340));
+        }
 
-            // for health circles
-            for (int i=50; i<=100 ; i+=25)
-            {
-                healthCircles.add(healthCircle(i, 340));
-            }
-
-             rectangle(620,100,10, 370,98,52,18);//dialogue box
+        rectangle(620, 100, 10, 370, 98, 52, 18);//dialogue box
 
 //            bottomColumnRectangle = rectangle(25, 120, 150, 211, 0, 0, 204, 0);//first try
 //            topColumnRectangle = rectangle(25, 120, 150, 9, 0, 0, 204, 0);//first try
@@ -47,25 +45,20 @@ public class BattleWindow implements Drawable{
 //        }
     }
 
-    private void columnUpdate(){
-        for(int i=140;i>500;i++ ){
-
-
-
-
-            for(int j=0;j>10;j++ ){
-                column( 140, 201);//bottom1
+    private void columnUpdate() {
+        for (int i = 140; i < 500; i++) {
+            for (int j = 0; j < 10; j++) {
+                column(140, 201);//bottom1
                 column(140, 9);//top1
 
-                column( 260, 201);//bottom2
-                column( 260, 9);//top2
+                column(260, 201);//bottom2
+                column(260, 9);//top2
 
-                column( 380, 201);//bottom3
-                column( 380, 9);//top3
+                column(380, 201);//bottom3
+                column(380, 9);//top3
 
                 column(500, 201);//bottom4
                 column(500, 9);//top4
-
             }
         }
 
@@ -73,19 +66,19 @@ public class BattleWindow implements Drawable{
         //toDraw.remove(column( 140, 201));
     }
 
-    private RectangleShape rectangle(int width, int height, int x, int y, int r, int g, int b){
-        Vector2f dimensions = new Vector2f(width,height);
+    private RectangleShape rectangle(int width, int height, int x, int y, int r, int g, int b) {
+        Vector2f dimensions = new Vector2f(width, height);
         RectangleShape Rectangle = new RectangleShape(dimensions);
         Rectangle.setPosition(x, y);
         Rectangle.setSize(dimensions);
-        Rectangle.setFillColor(new Color(r,g,b));
+        Rectangle.setFillColor(new Color(r, g, b));
         Rectangle.setOutlineThickness(1);
         toDraw.add(Rectangle);
         return Rectangle;
     }
 
-    private RectangleShape column ( int x, int y){
-        Vector2f dimensions = new Vector2f(25,130);
+    private RectangleShape column(int x, int y) {
+        Vector2f dimensions = new Vector2f(25, 130);
         RectangleShape Rectangle = new RectangleShape(dimensions);
         Rectangle.setPosition(x, y);
         Rectangle.setSize(dimensions);
@@ -106,7 +99,7 @@ public class BattleWindow implements Drawable{
 
     @Override
     public void draw(RenderTarget renderTarget, RenderStates renderStates) {
-        for(Drawable item : toDraw) {
+        for (Drawable item : toDraw) {
             renderTarget.draw(item);
         }
     }
