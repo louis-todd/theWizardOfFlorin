519788556837f0b5910cfacf027585c0d84866db
Chloe <chloetaylor@Cat.local>
2021-02-23 15:48:33 +0000

    Attempt to fix threading on linux issue

--- src/me/ghost/Main.java
+++ src/me/ghost/Main.java
@@ -2,6 +2,11 @@ package me.ghost;
 
 public class Main {
     public static void main(String[] args){
+
+        String OS = System.getProperty("os.name").toLowerCase();
+        if(OS.equals("linux")){
+            System.loadLibrary("fixXInitThreads");
+        }
         Game game = new Game();
         game.run();
     }
